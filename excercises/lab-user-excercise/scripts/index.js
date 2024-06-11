9af56d09cfb5164a29a47ca27671b1d67f953630
Sounketa <julien.ilboudo97@gmail.com>
2024-06-11 10:52:21 -0400

    Displaying user data in a table

---
+++ excercises/lab-user-excercise/scripts/index.js
@@ -0,0 +1,41 @@
+"use strict"
+
+const usersTableBody = document.querySelector("#usersTableBody");
+
+fetch("http://localhost:3000/users")
+  .then((response) => response.json())
+  .then((users) => {
+    console.log(users);
+    loadUsersTable(users);
+  });
+
+function loadUsersTable(users) {
+  usersTableBody.innerHTML = "";
+  for (const user of users) {
+    buildTableRow(user);
+  }
+}
+
+function buildTableRow(user) {
+  let row = usersTableBody.insertRow();
+
+  let cell1 = row.insertCell();
+  cell1.innerText = user.name;
+
+  let cell2 = row.insertCell();
+  cell2.innerText = user.username;
+
+  let cell3 = row.insertCell();
+  cell3.innerText = user.email;
+
+  let cell4 = row.insertCell();
+  cell4.innerText = user.phone;
+
+  let cell5 = row.insertCell();
+  cell5.innerText = user.website;
+
+  let cell6 = row.insertCell();
+  cell6.innerText = `${user.company.name}`;
+}
+
+
