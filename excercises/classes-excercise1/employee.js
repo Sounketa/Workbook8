25d0e2835e8f3c162854ade51b7482512ad7a0c5
Sounketa <julien.ilboudo97@gmail.com>
2024-06-17 12:51:40 -0400

    employee full name

--- excercises/classes-excercise1/employee.js
+++ excercises/classes-excercise1/employee.js
@@ -1,15 +1,27 @@
-"use strict"
+"use strict";
 
 class Employee {
-    constructor(id, firstName, lastName, jobTitle, payRate) {
+  constructor(id, firstName, lastName, jobTitle, payRate) {
     this.employeeId = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.jobTitle = jobTitle;
     this.payRate = payRate;
-    }
-   }
-   
-   let employee1 = new Employee(
-    1, "Ian", "Auston", "Graphic Artist", 42.50);
-   console.log(`Employee ${employee1.firstName} created`);
+  }
+
+  getFullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  getIntro() {
+    let intro =
+      "Hi! I'm " + this.getFullName() + " and I am a " + this.jobTitle;
+    return intro;
+  }
+}
+
+let employee1 = new Employee(1, "Ian", "Auston", "Graphic Artist", 42.5);
+console.log(`Employee ${employee1.firstName}`);
+console.log(`Job title is ${employee1.jobTitle}`);
+console.log(`Job title is $${employee1.payRate}`);
+console.log(employee1.getIntro());
